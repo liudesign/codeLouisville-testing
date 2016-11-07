1dbf21b306e481d1a27bb640aa871e30f9df43de
john.liu <john.liu@cdiniteractive.com>
2016-11-06 22:55:41 -0500

    added module 3 files

--- test/main_test.js
+++ test/main_test.js
@@ -2,8 +2,8 @@ var expect = require('chai').expect;
 
 // Test suite
 describe('Mocha', function () {
-	// Test spec (unit test)
-	it('should run our tests using npm', function () {
-		expect(true).to.be.ok;
-	});
+  //Test spec / unit test
+  it('should run our tests using npm', function () {
+    expect(true).to.be.ok;
+  });
 });
